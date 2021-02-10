cd16feb2f836fc5f9b67a454f3c76c894bb653c4
shubhamk0027 <shubhamk0027@gmail.com>
2021-02-10 23:31:01 +0530

    Added custom delimiter and only positive support

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,24 +1,49 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class StringCalculator {
 
     /**
      * Assumes the sum of the numbers lie in the range of int
-     * @param numbers is a string of integral numbers separated by comma or \n
+     * Assumes all the inputs provided are valid
+     * By default separates the numbers by comma or new line character
+     * Supports Custom Delimiter by providing input numbers string as “//[delimiter]\n[numbers…]”
+     * @param numbers is a string of positive integers numbers separated by comma or \n
      * @return the sum of the numbers
      */
     public int Add(String numbers){
-        String[] stringNumbers = numbers.split("[,\n]");
+
+        if(numbers.length()==0) {
+            return 0;
+        }
+
+        String[] stringNumbers;
+        if(numbers.startsWith("//")){
+            int index = numbers.indexOf('\n');
+            String newDelimiter = numbers.substring(2,index);
+            stringNumbers = numbers.substring(index+1).split(newDelimiter);
+        }else{
+            stringNumbers = numbers.split("[,\n]");
+        }
+
         int sum=0;
-        for(String number: stringNumbers) if(number.length()>0){
-            sum=sum+Integer.parseInt(number);
+        ArrayList<Integer> negatives = new ArrayList <>();
+        for(String number: stringNumbers) {
+            int value = Integer.parseInt(number);
+            if(value<0) {
+                negatives.add(value);
+            }
+            sum+=value;
+        }
+        if(negatives.size()>0){
+            throw new IllegalArgumentException("negatives not allowed! - "+negatives);
         }
         return sum;
     }
 
 
     public static void main(String[] args){
-        System.out.println("String Calculator");
-        StringCalculator stringCalculator= new StringCalculator();
-        stringCalculator.Add("");
+        System.out.println("String Calculator Running...");
     }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -31,6 +33,29 @@ public class StringCalculatorTest {
     @Test
     public void addMoreThanTwoStringsSeparatedByNewLineOrComma(){
         StringCalculator calculator = new StringCalculator();
-        assertEquals(calculator.Add("1\n2,3,4,\n5"),15);
+        assertEquals(calculator.Add("1\n2,3,4\n5"),15);
+    }
+
+    @Test
+    public void customDelimiterTest1(){
+        StringCalculator calculator= new StringCalculator();
+        assertEquals(calculator.Add("//;\n1;2;3"),6);
+    }
+
+    @Test
+    public void customDelimiterTest2(){
+        StringCalculator calculator= new StringCalculator();
+        assertEquals(calculator.Add("//__\n1__2__3"),6);
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void negativesTest(){
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("negatives not allowed! - [-4, -5, -6]");
+        StringCalculator calculator= new StringCalculator();
+        calculator.Add("1,2\n3,-4\n-5,-6");
     }
 }
