73b9b3092b7ad6284b67b79bc228cfa342d5bcc1
shubhamk0027 <shubhamk0027@gmail.com>
2021-02-10 22:54:56 +0530

    Added basic addition and parsing of numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,24 @@
 public class StringCalculator {
 
+    /**
+     * Assumes the sum of the numbers lie in the range of int
+     * @param numbers is a string of integral numbers separated by comma
+     * @return the sum of the numbers
+     */
     public int Add(String numbers){
+        String[] stringNumbers = numbers.split(",");
+        int sum=0;
+        for(String number: stringNumbers) if(number.length()>0){
+            sum=sum+Integer.parseInt(number);
+        }
+        return sum;
+    }
 
-        return 0;
+
+    public static void main(String[] args){
+        System.out.println("String Calculator");
+        StringCalculator stringCalculator= new StringCalculator();
+        stringCalculator.Add("");
     }
 
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
 
+    @Test
+    public void emptyStringTest(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.Add(""),0);
+    }
+
+    @Test
+    public void addOneStringTest(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.Add("1"),1);
+    }
+
+    @Test
+    public void addTwoStringTest(){
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(calculator.Add("1,2"),3);
+    }
 
 }
